5564e82b5b682f7a3787a64dd067ba83bedcc5ab
Jefferson Calmon <jeffjeff0197@gmail.com>
2020-12-10 23:05:27 -0200

    feat: Challenge completed

--- src/database/index.ts
+++ src/database/index.ts
@@ -8,7 +8,7 @@ export default async (name = 'default'): Promise<Connection> => {
       name,
       database:
         process.env.NODE_ENV === 'test'
-          ? 'd4ilocmcp55hub'
+          ? 'gostack_desafio06_tests'
           : defaultOptions.database,
     }),
   );
